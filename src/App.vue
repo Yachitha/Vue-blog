48a92ab3c3b76f56d06ce63f56e74ece79b277b7
Yachitha <yachithasandaruwan@gmail.com>
2018-04-24 06:25:21 +0530

    Initial commit of the blog

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <blog-header></blog-header>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  export default{
+    components:{
+      'blog-header':Header
+    },
+    data(){
+      return {
+        title: 'This is my first project!'
+      }
+    },
+    methods:{
+      greeting: function(){
+        return 'this is the body';
+      }
+    }
+  }
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="blog-header">
+        <div id="nav-items">
+            <ul>
+               <li><a href="#"><img src="../assets/logo.png" alt="logo"></a></li>
+               <li><a href="#">Home</a></li>
+               <li><a href="#">Who we are</a></li>
+               <li><a href="#">Blog</a></li> 
+            </ul>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #blog-header{
+        background-color: aquamarine;
+        height: 80px;
+        width: 100%;
+        position: fixed;
+        padding: 10px;
+    }
+    #nav-items  ul{
+        float: right;
+        position: relative;
+        margin-right: 50px;
+        display: block;
+    }
+    
+    #nav-items ul li{
+        display: inline;
+    }
+    #nav-items  ul  li  a{
+        padding: 0.5em 0.5em;
+        color: #020202;
+        text-decoration: none;
+        font-size: 20px;
+        position: relative;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    #nav-items img{
+        position: relative;
+        height:50px;
+        width: 50px;
+        padding: 0;
+    }
+    
+</style>
+
